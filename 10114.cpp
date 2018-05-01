9f4ff0e8e793209d249e995fac6ee9504e8c59ee
gchen <glenn.chen01@gmail.com>
2018-05-01 16:30:03 -0400

    add for loop

--- 10114.cpp
+++ 10114.cpp
@@ -6,53 +6,62 @@ using namespace std;
 
 void generate_rate(vector<double> &depreciation_rate)
 {
-  double previous_rate = depreciation_rate[0];
+    double previous_rate = depreciation_rate[0];
 
-  for (int i=0; i<depreciation_rate.size(); ++i) {
-    if (depreciation_rate[i] == 0) {
-      depreciation_rate[i] = previous_rate;
+    for (int i=0; i<depreciation_rate.size(); ++i) {
+        if (depreciation_rate[i] == 0) {
+            depreciation_rate[i] = previous_rate;
+        }
+        previous_rate = depreciation_rate[i];
     }
-    previous_rate = depreciation_rate[i];
-  }
 }
 
 
 int main() {
-  int months,depreciation_month, depreciation_records_count;
-  double  down_payment, total, depreciation_rate, buyer_owes, current_car_value, monthly_payment;
-  vector<double> monthly_deprecation_rate;
-  cin >> months >> down_payment >> total >> depreciation_records_count;
-  if (months > 0) {
-    // array<int, months> monthly_deprecation_rate; //error array size not known at compile time
+    while (true) {
+        int months,depreciation_month, depreciation_records_count;
+        double  down_payment, total, depreciation_rate, buyer_owes, current_car_value, monthly_payment;
+        vector<double> monthly_deprecation_rate;
+        cin >> months >> down_payment >> total >> depreciation_records_count;
+        if (months > 0) {
+            // array<int, months> monthly_deprecation_rate; //error array size not known at compile time
 
-    for (int i=0; i< months; ++i) {
-      //monthly_deprecation_rate[i] = 0;
-      monthly_deprecation_rate.push_back(0.0);
-    }
-    for (int i=0; i<depreciation_records_count; ++i) {
-      cin >> depreciation_month >> depreciation_rate;
-      monthly_deprecation_rate[depreciation_month] =  depreciation_rate;
-    }
-  }
+            for (int i = 0; i < months; ++i) {
+                //monthly_deprecation_rate[i] = 0;
+                monthly_deprecation_rate.push_back(0.0);
+            }
+            for (int i = 0; i < depreciation_records_count; ++i) {
+                cin >> depreciation_month >> depreciation_rate;
+                monthly_deprecation_rate[depreciation_month] = depreciation_rate;
+            }
 
-  generate_rate(monthly_deprecation_rate);
+            generate_rate(monthly_deprecation_rate);
 
-  current_car_value = total + down_payment; // Note: the description told you car total value
-  buyer_owes = total;
-  monthly_payment = total/months; // Note: the description does not tell you monthly payment
-  for (int i=0; i<months; ++i) {
-    current_car_value = current_car_value * (1.0- monthly_deprecation_rate[i]);
-    buyer_owes = buyer_owes - monthly_payment;
-    if (buyer_owes < current_car_value ) {
-      cout << i + 1;
-      if (i+ 1>1) {
-	cout << " months";
-      } else {
-	cout << " month" ;
-      }
-      cout << endl;
-      break;
+            current_car_value = total + down_payment; // Note: the description told you car total value
+            buyer_owes = total;
+            monthly_payment = total / months; // Note: the description does not tell you monthly payment
+            for (int i = 0; i < months; ++i) {
+                current_car_value = current_car_value * (1.0 - monthly_deprecation_rate[i]);
+                buyer_owes = buyer_owes - monthly_payment;
+                if (buyer_owes < current_car_value) {
+                    if (i == 0) {
+                        // Note: special case, right at the point when the car is bought
+                        cout << "0 month" << endl;
+                    } else {
+                        cout << i + 1;
+                        if (i + 1 > 1) {
+                            cout << " months" << endl;
+                        } else {
+                            cout << " month" << endl;
+                        }
+                    }
+                    break;
+                }
+            }
+        } else {
+            break;
+        }
     }
-  }
 }
+
 
