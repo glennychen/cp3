cbba8ee8aaad79d9b0e67df353ee08c838afcc52
gchen <glenn@secondform.com>
2018-05-12 14:53:29 -0400

    10071 - Back to High School Physics

---
+++ uva/10071.cpp
@@ -0,0 +1,12 @@
+// 10071 - Back to High School Physics
+#include <iostream>
+using namespace std;
+
+int main() {
+  int v, t;
+  while(scanf("%d %d", &v, &t) != EOF) {
+    cout << 2*v*t << endl;
+  }
+
+  return 0;
+}
