e10dc3d6327d23b9e3e4bdf213a6dac066753307
gchen <glenn@secondform.com>
2018-05-17 08:10:33 -0400

    add uva 100 3n+1

---
+++ uva/100.cpp
@@ -0,0 +1,48 @@
+// UVA 100 - The 3n + 1 problem
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int three_n_plus_1(int n)
+{
+  int input = n;
+  int count = 1;
+
+  while(n!=1) {
+    ++count;
+    if(n%2==1) {
+      n = 3*n+1;
+    } else {
+      n = n/2;
+    }
+  }
+  // cout << input; //DEBUG
+  //cout << " count: " << count << endl; //DEBUG
+  return count;
+}
+
+int main() {
+  for(;;){
+    int i, j, max_cycle;
+    cin >> i >> j;
+    cout << i << " " << j;
+    int tmp;
+    if (j<i) {
+      i = tmp;
+      j = i;
+      i = tmp;
+    }
+
+    max_cycle = 0;
+    for(int k=i; k<=j; ++k){
+      int cycle = three_n_plus_1(k);
+      if (cycle>max_cycle) {
+	max_cycle = cycle;
+      }
+    }
+    cout << " " << max_cycle << endl;
+
+  }
+  return 0;
+}
