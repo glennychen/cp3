4bdfa33f639b4769afb5a320328d9309379c434b
gchen <glenn.chen01@gmail.com>
2018-05-18 06:58:14 -0400

    3n+1

--- uva/100.cpp
+++ uva/100.cpp
@@ -1,4 +1,4 @@
-// UVA 100 - The 3n + 1 problem
+// UVA 100 - The 3n + 1 problem                                                                                                                                                                                                              
 #include <iostream>
 #include <vector>
 
@@ -6,43 +6,46 @@ using namespace std;
 
 int three_n_plus_1(int n)
 {
-  int input = n;
-  int count = 1;
+    int input = n;
+    int count = 1;
 
-  while(n!=1) {
-    ++count;
-    if(n%2==1) {
-      n = 3*n+1;
-    } else {
-      n = n/2;
+    while(n!=1) {
+        ++count;
+        if(n%2==1) {
+            n = 3*n+1;
+        } else {
+            n = n/2;
+        }
     }
-  }
-  // cout << input; //DEBUG
-  //cout << " count: " << count << endl; //DEBUG
-  return count;
+    // cout << input; //DEBUG
+    //cout << " count: " << count << endl; //DEBUG
+    return count;
 }
 
 int main() {
-  for(;;){
-    int i, j, max_cycle;
-    cin >> i >> j;
-    cout << i << " " << j;
-    int tmp;
-    if (j<i) {
-      i = tmp;
-      j = i;
-      i = tmp;
-    }
+    for(;;){
+        int i, j, max_cycle;
+        cin >> i;
+        if (cin.fail()) break;
+        cin >> j;
+        cout << i << " " << j;
+        int tmp;
+        if (j<i) {
+            tmp = i;
+            i=j;
+            j = tmp;
+        }
 
-    max_cycle = 0;
-    for(int k=i; k<=j; ++k){
-      int cycle = three_n_plus_1(k);
-      if (cycle>max_cycle) {
-	max_cycle = cycle;
-      }
-    }
-    cout << " " << max_cycle << endl;
+        max_cycle = 0;
+        for(int k=i; k<=j; ++k){
+            int cycle = three_n_plus_1(k);
+            if (cycle>max_cycle) {
+                max_cycle = cycle;
+            }
+        }
+        cout << " " << max_cycle << endl;
 
-  }
-  return 0;
+    }
+    return 0;
 }
+
