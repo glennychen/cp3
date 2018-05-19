79a65763167fe4131004f00bf950de07006fc850
gchen <glenn.chen01@gmail.com>
2018-05-18 22:18:51 -0400

    add 272

---
+++ uva/272.cpp
@@ -0,0 +1,39 @@
+// 272 - TEX Quotes
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  string line;
+  bool first_quote = true;
+  while(getline(cin, line)){
+    for(int i=0;i<line.size();++i){
+      if(line[i]=='"'){
+	if (first_quote) {
+	  cout << "``";
+	} else {
+	  cout << "\'\'";
+	}
+	first_quote = !first_quote;
+      } else {
+	cout << line[i];
+      }
+    }
+    cout << endl;
+    //        size_t found = line.find("\"");
+    //        if (found != string::npos) {
+    //            if(first_quote) {
+    //                line[found] = '`';
+    //                line.insert(found, "`"); // not inserting char but string?
+    //                first_quote = false;
+    //            } else {
+    //                line[found] = '\'';
+    //                line.insert(found, "'");
+    //                first_quote = true;
+    //            }
+    //        }
+    //
+    //        cout << line << endl;
+  }
+  return 0;
+}
