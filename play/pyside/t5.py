a697aaf919a30ebfd79f6e0a27af06224782404f
gchen <glenn@secondform.com>
2018-07-01 17:13:38 -0400

    add timer

--- play/pyside/t5.py
+++ play/pyside/t5.py
@@ -2,6 +2,7 @@
 
 
 import sys
+from datetime import datetime
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
@@ -12,22 +13,33 @@ class MyWidget(QtWidgets.QWidget):
         quit = QtWidgets.QPushButton("Quit")
         quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
 
-        lcd = QtWidgets.QLCDNumber(2)
-
-        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        slider.setRange(0, 99)
-        slider.setValue(0)
+        self.counter = 11
+        
+        self.lcd = QtWidgets.QLCDNumber(2)
+        self.lcd.display(5);
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setRange(0, 99)
+        self.slider.setValue(0)
 
         self.connect(quit, QtCore.SIGNAL("clicked()"),
                      QtWidgets.qApp, QtCore.SLOT("quit()"))
-        self.connect(slider, QtCore.SIGNAL("valueChanged(int)"),
-                     lcd, QtCore.SLOT("display(int)"))
+        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
+                     self.lcd, QtCore.SLOT("display(int)"))
 
+        self.timer = QtCore.QTimer(self)
+        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.setValue)
+        self.timer.start(1000)
+        
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(quit);
-        layout.addWidget(lcd);
-        layout.addWidget(slider);
+        layout.addWidget(self.lcd);
+        layout.addWidget(self.slider);
         self.setLayout(layout);
+
+    @QtCore.Slot()    
+    def setValue(self):
+        self.counter -= 1
+        self.lcd.display(self.counter)
 
 
 app = QtWidgets.QApplication(sys.argv)
