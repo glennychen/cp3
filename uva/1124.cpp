3f9975f25d831c5a9f1c3d41f93c2aee3b0bc25c
gchen <glenn.chen01@gmail.com>
2019-02-17 22:34:52 -0500

    UVa 01124 - Celebrity Jeopardy

---
+++ uva/1124.cpp
@@ -0,0 +1,12 @@
+// UVa 01124 - Celebrity Jeopardy 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string input;
+  while(getline(cin, input)) {
+    cout << input << endl;
+  }
+}
