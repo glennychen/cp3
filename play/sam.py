b6ae9faad2cfb02489db7d5e7e50b51558ef4859
gchen <glenn.chen01@gmail.com>
2019-02-27 12:49:08 -0500

    update sam.py

--- play/sam.py
+++ play/sam.py
@@ -5,13 +5,14 @@ while True:
     if ultimate_count == 4:
         print("Use 4 ultimates")
         break
-    counter +=1
-    print(counter )
     time.sleep(1)
+    counter +=1
+    print(counter)
     if counter%54 == 0:
         print("Heal")
     if counter%56 == 0:
-        ultimate_count += 1
         print("ULTIMATE:"+ str(ultimate_count))
+        print('\a')
+        ultimate_count += 1
         counter = 0
         
