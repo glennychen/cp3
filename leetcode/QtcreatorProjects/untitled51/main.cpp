e5f03340780be0040be7b3050f548570fa098c85
gchen <glenn.chen01@gmail.com>
2021-03-09 21:44:56 -0500

    update

---
+++ leetcode/QtcreatorProjects/untitled51/main.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/build-an-array-with-stack-operations/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+
+    }
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
