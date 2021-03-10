d71f40d5645ae634f6239136454c6fee97bbef46
gchen <glenn.chen01@gmail.com>
2021-03-10 17:55:15 -0500

    update

--- leetcode/QtcreatorProjects/untitled54/main.cpp
+++ leetcode/QtcreatorProjects/untitled54/main.cpp
@@ -26,6 +26,32 @@ vector<int> shortestToChar(string s, char c) {
     return output;
 }
 
+vector<int> O_of_n_shortestToChar(string S, char C) {
+           int n = S.size(), pos = -n;
+           vector<int> res(n, n);
+
+
+           for (int i = 0; i < n; ++i) {
+               if (S[i] == C)
+                   pos = i;
+               res[i] = i - pos; //think i is on the right (i.e. larger) of pos(pos is the target)
+                                 //so i-pos is the distance.
+                                 //for example, we found target before (=pos), now the later i position
+                                 // is i-pos away from pos.
+           }
+           for (int i = pos - 1; i >= 0; --i) {
+               if (S[i] == C)
+                   pos = i;
+
+                                //think i is on the left (i.e. smaller) of pos(pos is the target)
+                                //so i-pos is the distance
+                                //for example, we found target before (=pos), now later i position
+                                // is pos-i away from pos
+               res[i] = min(res[i], pos - i);
+           }
+           return res;
+    }
+
 TEST_CASE( "Integer to Roman") {
     vector<int> v{3,2,1,0,1,0,0,1,2,2,1,0};
     REQUIRE( shortestToChar("loveleetcode", 'e') == v  );
@@ -35,4 +61,7 @@ TEST_CASE( "Integer to Roman") {
 
     vector<int> v3 = {4,3,2,1,0,1,2,3,4,5};
     REQUIRE( shortestToChar("xuzmnimdwf", 'n') == v3 );
+
+    vector<int> v4{3,2,1,0,1,0,0,1,2,2,1,0};
+    REQUIRE( O_of_n_shortestToChar("loveleetcode", 'e') == v4  );
 }
