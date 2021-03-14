4e921688a021ed9cc7529a06e78c5da8df484c44
gchen <glenn.chen01@gmail.com>
2021-03-14 19:27:51 -0400

    Update

--- leetcode/QtcreatorProjects/untitled65/main.cpp
+++ leetcode/QtcreatorProjects/untitled65/main.cpp
@@ -1,9 +1,33 @@
+#define CATCH_CONFIG_MAIN
+#include<catch2/catch_all.hpp>
+#include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+//https://leetcode.com/problems/how-many-apples-can-you-put-into-the-basket/
+
+int maxNumberOfApples(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    constexpr int MAX=5000;
+    int count=0;
+    int total=0;
+    for(const auto& elem: arr){
+        total+=elem;
+        if(total>MAX){
+            break;
+        }
+        ++count;
+    }
+    return count;
+}
+
+TEST_CASE( "maxNumberOfApples") {
+    vector<int> v{100,200,150,1000};
+    REQUIRE( maxNumberOfApples(v) == 4 );
+
+    v={900,950,800,1000,700,800};
+    REQUIRE( maxNumberOfApples(v) == 5 );
 }
