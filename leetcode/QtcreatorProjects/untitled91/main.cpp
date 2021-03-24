ebf591d0ee2a84e769285f22e6b19a4a283f87a8
gchen <glenn.chen01@gmail.com>
2021-03-23 20:59:22 -0400

    update

--- leetcode/QtcreatorProjects/untitled91/main.cpp
+++ leetcode/QtcreatorProjects/untitled91/main.cpp
@@ -3,8 +3,27 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
 class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int low=0;
+       int high=nums.size()-1;
+
+       while(low<high){
+           int sum=nums[low]+nums[high];
+           if (sum==target)
+               return vector<int>{low+1, high+1}; //+1 because the quest requires 1-base index
+           else if(sum>target){
+               --high;
+           } else if(sum<target){
+               ++low;
+           }
+       }
+
+        return vector<int>{};
+    }
+ };
+class Solution2_hashing {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_multimap<int, int> um;
@@ -16,12 +35,9 @@ public:
             int goal=target-nums[i];
             auto range=um.equal_range(goal);
             auto iter=range.first;
-            for(;iter!=range.second;++iter){
-                if(iter->second==i){
-                    //remember to check duplicate
-
-                    return vector<int>{i, iter->second};
-                }
+            for(;iter!=range.second&&iter->second!=i;++iter){
+                //remember to check iter->second!=i, do not use the fixed element, need to find the one "after" index i
+               return vector<int>{i, iter->second};
             }
 
         }
