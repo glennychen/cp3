d6af9545491012a338572fce45d15937e726ac9d
gchen <glenn.chen01@gmail.com>
2021-03-24 22:04:55 -0400

    Update

--- leetcode/QtcreatorProjects/untitled94/main.cpp
+++ leetcode/QtcreatorProjects/untitled94/main.cpp
@@ -18,6 +18,7 @@ public:
          vector<int> aa;
         for(int i=0;i<B.size();++i){
             auto iter=mm.lower_bound(B[i]+1);
+            bool found=false;
             if(iter->second!=0){
                 //a lower bound is found
                 while(iter->second==1&&iter!=mm.end()){
@@ -26,14 +27,18 @@ public:
                 if(iter!=mm.end()){
                     aa.push_back(iter->first);
                     iter->second=1; //used
+                    found=true;
                 }
-            } else { /*if(iter->second==0){*/
+            }
+
+            if(!found){
                 //lower bound not found
                 //pick the smallest that is not used
                 for(auto& elem:mm){
                     if(elem.second==-1){
                         aa.push_back(elem.first);
                         elem.second=1; //used
+                        break;
                     }
                 }
             }
@@ -49,3 +54,12 @@ int main()
     cout << "Hello World!" << endl;
     return 0;
 }
+
+[2,0,4,1,2]
+[1,3,0,0,2]
+
+[2,0,4,1,2]
+[1,3,0,0,2]
+
+[15448,14234,13574,19893,6475]
+[14234,6475,19893,15448,13574]
