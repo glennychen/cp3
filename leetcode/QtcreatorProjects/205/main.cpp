459750791ab5ef0ef54c24bd2903eef531d76bd5
glenn <glenn@asus.com>
2022-04-27 16:46:15 -0400

    add 205

---
+++ leetcode/QtcreatorProjects/205/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> um;
+        unordered_map<char,char> um2;
+
+        for(int i=0;i<s.size();++i){
+            if(um.find(s[i])==um.end()){
+                if(um2.find(t[i])==um2.end()){
+                    um[s[i]]=t[i];
+                    um2[t[i]]=t[i];
+                } else {
+                    return false; //cannot reuse the substitution
+                }
+            } else {
+                if(um[s[i]]!=t[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
